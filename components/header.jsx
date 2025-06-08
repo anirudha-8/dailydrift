bd539338371d8dfb6ce24ea921ef9263c8ed6e81
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-08 13:01:27 +0530

    refactor: change app's logo

--- components/header.jsx
+++ components/header.jsx
@@ -8,47 +8,47 @@ import UserMenu from "./user-menu";
 import { checkUser } from "@/lib/checkUser";
 
 async function Header() {
-  await checkUser();
+	await checkUser();
 
-  return (
-    <header className="container mx-auto">
-      <nav className="py-6 px-4 flex justify-between items-center">
-        <Link href="/">
-          <Image
-            src={"/logo.png"}
-            alt="Reflct Logo"
-            width={200}
-            height={60}
-            className="h-10 w-auto object-contain"
-          />
-        </Link>
-        <div className="flex items-center gap-4">
-          <SignedIn>
-            <Link href="/dashboard#collections">
-              <Button variant="outline" className="flex items-center gap-2">
-                <FolderOpen size={18} />
-                <span className="hidden md:inline">Collections</span>
-              </Button>
-            </Link>
-          </SignedIn>
-          <Link href="/journal/write">
-            <Button variant="journal" className="flex items-center gap-2">
-              <PenBox size={18} />
-              <span className="hidden md:inline">Write New</span>
-            </Button>
-          </Link>
-          <SignedOut>
-            <SignInButton forceRedirectUrl="/dashboard">
-              <Button variant="outline">Login</Button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <UserMenu />
-          </SignedIn>
-        </div>
-      </nav>
-    </header>
-  );
+	return (
+		<header className="container mx-auto">
+			<nav className="py-6 px-4 flex justify-between items-center">
+				<Link href="/">
+					<Image
+						src={"/DailyDriftLogo.png"}
+						alt="Reflct Logo"
+						width={400}
+						height={300}
+						className="h-14 w-auto object-contain"
+					/>
+				</Link>
+				<div className="flex items-center gap-4">
+					<SignedIn>
+						<Link href="/dashboard#collections">
+							<Button variant="outline" className="flex items-center gap-2">
+								<FolderOpen size={18} />
+								<span className="hidden md:inline">Collections</span>
+							</Button>
+						</Link>
+					</SignedIn>
+					<Link href="/journal/write">
+						<Button variant="journal" className="flex items-center gap-2">
+							<PenBox size={18} />
+							<span className="hidden md:inline">Write New</span>
+						</Button>
+					</Link>
+					<SignedOut>
+						<SignInButton forceRedirectUrl="/dashboard">
+							<Button variant="outline">Login</Button>
+						</SignInButton>
+					</SignedOut>
+					<SignedIn>
+						<UserMenu />
+					</SignedIn>
+				</div>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
