741fa54a5eab6bbc6a0e1967381d36cf3ea6c2a2
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-13 13:19:02 +0530

    bug: fix bug issue

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,14 @@ import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
 import { Toaster } from "sonner";
 import "react-quill-new/dist/quill.snow.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Reflct",
+	title: "DailyDrift",
 	description: "",
 };
 
@@ -16,19 +17,19 @@ export default function RootLayout({ children }) {
 	return (
 		<ClerkProvider
 		// appearance={{
-		//   baseTheme: shadesOfPurple,
-		//   variables: {
-		//     colorPrimary: "#3b82f6",
-		//     colorBackground: "#1a202c",
-		//     colorInputBackground: "#2D3748",
-		//     colorInputText: "#F3F4F6",
-		//   },
-		//   elements: {
-		//     formButtonPrimary: "bg-purple-600 hover:bg-purple-700 text-white",
-		//     card: "bg-gray-800",
-		//     headerTitle: "text-blue-400",
-		//     headerSubtitle: "text-gray-400",
-		//   },
+		// 	baseTheme: shadesOfPurple,
+		// 	variables: {
+		// 		colorPrimary: "#3b82f6",
+		// 		colorBackground: "#1a202c",
+		// 		colorInputBackground: "#2D3748",
+		// 		colorInputText: "#F3F4F6",
+		// 	},
+		// 	elements: {
+		// 		formButtonPrimary: "bg-purple-600 hover:bg-purple-700 text-white",
+		// 		card: "bg-gray-800",
+		// 		headerTitle: "text-blue-400",
+		// 		headerSubtitle: "text-gray-400",
+		// 	},
 		// }}
 		>
 			<html lang="en">
